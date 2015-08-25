6bef712fff7b077e769f4e6f47d0d3854b3f38df
makotot <makoto.ttn@gmail.com>
2015-08-25 13:13:26 +0900

    0.0.3

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var path = require('path'),
   fs = require('fs'),
   stripComments = require('strip-json-comments');
 
-module.exports = function () {
-  var rc = path.resolve(__dirname, '.eslintrc');
+var rc = path.resolve(__dirname, '.eslintrc');
 
-  return stripComments(fs.readFileSync(rc, 'utf-8'));
-};
+module.exports = JSON.parse(stripComments(fs.readFileSync(rc, 'utf-8')));
+
