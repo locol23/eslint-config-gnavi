df2020a162628e5545c154cc5397783c07f1ddb2
Ryo Akiyama <akiyama-r@gnavi.co.jp>
2018-12-10 11:37:31 +0900

    airbnb-base を extends しつつ環境構成を最新のものに刷新

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 module.exports = {
-  extends: [
-    './rules/best-practices',
-    './rules/errors',
-    './rules/node',
-    './rules/style',
-    './rules/variables',
-    './rules/es6',
-    './rules/imports',
-  ].map(require.resolve),
+  extends: ['airbnb-base'],
+  parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 2017,
+    ecmaVersion: 2019,
     sourceType: 'module',
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true,
-    },
   },
   rules: {
-    strict: 'error',
+    semi: ['error', 'never'],
+    'max-len': ['error', { code: 120 }],
   },
 }
