f8070299bbd950ed36f559845790abec3b9e73d6
Ryo Akiyama <akiyama-r@gnavi.co.jp>
2019-02-27 18:17:14 +0900

    検討済み追加ルール適用

--- index.js
+++ index.js
@@ -8,5 +8,14 @@ module.exports = {
   rules: {
     semi: ['error', 'never'],
     'max-len': ['error', { code: 120 }],
+    camelcase: ['error', { ignoreDestructuring: true }],
+    'prefer-object-spread': ['error'],
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-invalid-this': 'off',
+    'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: [] }],
+    'no-restricted-syntax': ['error', 'ForInStatement', 'LabeledStatement', 'WithStatement'],
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': 'error',
+    'import/no-named-default': 'off',
   },
 }
