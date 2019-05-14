41d5657bf6215568a2cc9f1fc1ac3d30caa2d458
Yoshitaka Terazawa <locol6622@gmail.com>
2019-05-14 12:26:26 +0900

    feat: tweak rules

--- packages/eslint-config-gnavi-typescript/index.js
+++ packages/eslint-config-gnavi-typescript/index.js
@@ -6,7 +6,15 @@ module.exports = {
   },
   plugins: ['@typescript-eslint'],
   rules: {
+    'arrow-parens': 'off',
+    'no-useless-constructor': 'off',
+    'no-empty-function': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/prefer-interface': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
+  settings: {
+    'import/extensions': ['.ts', '.tsx'],
   },
 }
